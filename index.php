6e6a0f593b3a86bb741901c5b97522ae55178758
pksv <pushkalvaidya1@gmail.com>
2019-09-29 18:02:07 +0530

    stage 4 commit

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
-<!DOCTYPE HTML>
-<html>
+<?php
 
+echo ' <!DOCTYPE HTML>
+<html>
 <head>
 	<title>MusicPlayer::Login</title>
 	<script>
@@ -11,11 +12,7 @@
 		function hideURLbar() {
 			window.scrollTo(0, 1);
 		}
-		function clean() {
-			document.getElementById("form1").reset();
-			document.getElementById("form2").reset();
-			document.getElementById("form3").reset();
-		}
+		
 	</script>
 	<link rel="stylesheet" href="css/login.css" type="text/css" media="all" />
 	<link href="css/font-awesome.min.css" rel="stylesheet">
@@ -34,7 +31,7 @@
 					<input type="radio" name="sections" id="option1" checked>
 					<label for="option1" class="icon-left-w3pvt"><span class="fa fa-user-circle" aria-hidden="true"></span>Login</label>
 					<article>
-						<form id='form1'action="home.php" method="post">							
+						<form id="form1" action="home.php" method="post">							
 						
 						
 						
@@ -47,7 +44,7 @@
 								<span class="fa fa-key" aria-hidden="true"></span>
 								<input type="password" placeholder="Password" name="password" required  autocomplete="off"/>
 							</div>
-							<button type="submit" class="btn submit" onClick="clean()">Login</button>
+							<button type="submit" class="btn submit" >Login</button>
 							<a href="#" class="bottom-text-w3ls">Forgot Password?</a>
 						</form>
 					</article>
@@ -56,21 +53,21 @@
 					<input type="radio" name="sections" id="option2">
 					<label for="option2" class="icon-left-w3pvt"><span class="fa fa-pencil-square" aria-hidden="true"></span>Register</label>
 					<article>
-						<form id='form2' action="sql/register.php" method="post">
+						<form id="form2" action="sql/register.php" method="post">
 							<h3 class="legend">Register Here</h3>
 							<div class="input">
 								<span class="fa fa-envelope-o" aria-hidden="true"></span>
-								<input type="email" placeholder="Email" name="email" required  autocomplete="off"/>
+								<input type="email" placeholder="Email" name="email" required  autocomplete="off" value=""/>
 							</div>
 							<div class="input">
 								<span class="fa fa-key" aria-hidden="true"></span>
-								<input type="password" placeholder="Password" name="password" required  autocomplete="off"/>
+								<input type="password" placeholder="Password" name="password" required  autocomplete="off" value=""/>
 							</div>
 							<div class="input">
 								<span class="fa fa-key" aria-hidden="true"></span>
-								<input type="password" placeholder="Confirm Password" name="password" required  autocomplete="off"/>
+								<input type="password" placeholder="Confirm Password" name="password" required  autocomplete="off" value=""/>
 							</div>
-							<button type="submit" class="btn submit" onClick="clean()" >Register</button>
+							<button type="submit" class="btn submit" >Register</button>
 						</form>
 					</article>
 				</div>
@@ -78,16 +75,16 @@
 					<input type="radio" name="sections" id="option3">
 					<label for="option3" class="icon-left-w3pvt"><span class="fa fa-lock" aria-hidden="true"></span>Forgot Password?</label>
 					<article>
-						<form id='form3' action="#" method="post">
+						<form id="form3" action="#" method="post">
 							<h3 class="legend last">Reset Password</h3>
-							<p class="para-style">Enter your email address below and we'll send you an email with instructions.</p>
+							<p class="para-style">Enter your email address below and we will send you an email with instructions.</p>
 							<p class="para-style-2"><strong>Need Help?</strong> Learn more about how to <a href="#">retrieve an existing
 									account.</a></p>
 							<div class="input">
 								<span class="fa fa-envelope-o" aria-hidden="true"></span>
 								<input type="email" placeholder="Email" name="email" required />
 							</div>
-							<button type="submit" class="btn submit last-btn" onClick="clean()">Reset</button>
+							<button type="submit" class="btn submit last-btn" >Reset</button>
 						</form>
 					</article>
 				</div>
@@ -98,4 +95,5 @@
 
 </body>
 
-</html>
+</html>';
+?>
--- sql/register.php
+++ sql/register.php
@@ -2,9 +2,9 @@
 	include "connection.php";
 	$email = $_POST["email"];
 	$pass = $_POST["password"];
-	$query = "insert into user values("$email","$pass");";
+	$query = "insert into user values('$email','$pass');";
 	mysqli_query($db, $query) or die("Error querying database.");		
-	echo "<script> window.location="http://localhost/musicplayer/login.php"; </script>";
+	echo "<script> window.location='http://localhost/musicplayer/login.php'; </script>";
     exit;
 	//ob_start();
 	//header("Location: C:\wamp64\www\ trial1\login.php");
